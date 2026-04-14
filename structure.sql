da7162de7a87b2409ab4d1cd9820f9a3d5d99b3d
Hamadellah <hamadellahotman13@gmail.com>
2026-04-14 16:11:02 +0100

    create school_db and define roles and users tables

--- structure.sql
+++ structure.sql
@@ -1,1 +1,15 @@
 create DATABASE school_db;
+use school_db;
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    label VARCHAR(50) NOT NULL
+);
+CREATE TABLE users(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(55),
+    last_name VARCHAR(55),
+    email VARCHAR(150) UNIQUE NOT NULL,
+    password VARCHAR(155) NOT NULL,
+    role_id INT ,
+    FOREIGN KEY (role_id) REFERENCES roles(id)
+);
